cdfed75267e73ef05776ce2281984baa48a4ff1c
unknown <kelvinagbonze@gmail.com>
2021-01-10 18:39:26 +0100

    fix error

--- pages/nigeria/Thisday.js
+++ pages/nigeria/Thisday.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { ThisdayCurrentNews } from "../../components/localNews/thisday/ThisdayCurrentNews";
-import { ThisdayLatestNewsList } from "../../components/localNews/thisday/ThisdayLatestNewsList";
+// import React from "react";
+// import { ThisdayCurrentNews } from "../../components/localNews/thisday/ThisdayCurrentNews";
+// import { ThisdayLatestNewsList } from "../../components/localNews/thisday/ThisdayLatestNewsList";
 
-import { GlobalProvider } from "../../Context/localNews/thisday/ThisdayGlobalState";
+// import { GlobalProvider } from "../../Context/localNews/thisday/ThisdayGlobalState";
 
-export default function Thisday() {
-	return (
-		<GlobalProvider>
-			<div className='container'>
-				<div className='row'>
-					<div className='col-md-8'>
-						<ThisdayCurrentNews />
-						<ThisdayLatestNewsList />
-					</div>
-					<div className='col-md-4'></div>
-				</div>
-			</div>
-		</GlobalProvider>
-	);
-}
+// export default function Thisday() {
+// 	return (
+// 		<GlobalProvider>
+// 			<div className='container'>
+// 				<div className='row'>
+// 					<div className='col-md-8'>
+// 						<ThisdayCurrentNews />
+// 						<ThisdayLatestNewsList />
+// 					</div>
+// 					<div className='col-md-4'></div>
+// 				</div>
+// 			</div>
+// 		</GlobalProvider>
+// 	);
+// }
